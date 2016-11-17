fe298e0d3af3e8b1534afa337c4abd0e342b8825
JuliKroiter <kroiter09@gmail.com>
2016-11-17 19:07:34 +0200

    flash

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+  def flash_class(type)
+    valid_class = case type
+                    when 'notice'
+                      'info'
+                    when 'error'
+                      'danger'
+                    when 'success'
+                      'success'
+                    else
+                      'warning'
+    end
+
+    "alert alert-#{valid_class}"
+  end
 end
