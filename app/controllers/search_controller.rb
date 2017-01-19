095258091fbeb8b641a3e2e121f45c43dca62e5c
liuda <liuda315@gmail.com>
2017-01-19 15:30:56 +0200

    remove reindex and fix css

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,9 +1,5 @@
 class SearchController < ApplicationController
   def search
-    Post.reindex
-    Comment.reindex
-    Category.reindex
-    Book.reindex
     term = params[:term]
     @posts = Post.search term
     @comments = Comment.search term
