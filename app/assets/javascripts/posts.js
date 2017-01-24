6a6fff7a4aa873aad30555227b87594f3b111637
liuda <liuda315@gmail.com>
2017-01-24 15:56:32 +0200

    fix categories

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -1,33 +1,33 @@
 $(document).ready(function() {
-  $('.i-category').click(function(){
-    var category = $(this).find('input').val();
+  $('.i-topic').click(function(){
+    var topic = $(this).find('input').val();
     // reset all categories
-    $('.i-category').removeClass('active-category');
-    $('.i-category').find('.category-title').css('color', '#656565');
-    $('.topic').removeClass('active-topic');
-    $('.all').addClass('active-topic');
+    $('.i-topic').removeClass('active-topic');
+    $('.i-topic').find('.topic-title').css('color', '#656565');
+    $('.category').removeClass('active-category');
+    $('.all').addClass('active-category');
     $('.act').hide();
     $('.def').show();
     // make active clicked category
     $(this).find('.def').hide();
     $(this).find('.act').show();
-    $(this).find('.category-title').css('color', '#d3215d');
-    $(this).addClass('active-category');
+    $(this).find('.topic-title').css('color', '#d3215d');
+    $(this).addClass('active-topic');
     $.ajax({
       type: 'GET',
-      url: '/posts?category=' + category,
+      url: '/posts?topic=' + topic,
       dataType: 'script'
     })
   })
 
-  $('.topic').click(function(){
-    var topic = $(this).find('input').val() == undefined ? '' : $(this).find('input').val()
-    var category = $('.active-category').find('input').val();
-    $('.topic').removeClass('active-topic');
-    $(this).addClass('active-topic');
+  $('.category').click(function(){
+    var category = $(this).find('input').val() == undefined ? '' : $(this).find('input').val()
+    var topic = $('.active-topic').find('input').val();
+    $('.category').removeClass('active-category');
+    $(this).addClass('active-category');
     $.ajax({
       type: 'GET',
-      url: '/posts?topic=' + topic + '&category=' + category,
+      url: '/posts?category=' + category + '&topic=' + topic,
       dataType: 'script'
     })
   })
